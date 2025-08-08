72ebc23340ddaee3a199074c791d84215e6c6959
AlanRyvoDevelopment <alanryvodevelopment@mail.ru>
2025-08-08 16:46:05 +0300

    Решил merge conflict после stash pop

--- crm-local/vite.config.js
+++ crm-local/vite.config.js
@@ -2,10 +2,22 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  server: {
+    host: '0.0.0.0',       // слушать все интерфейсы
+    port: 5173,
+    strictPort: true,
+    cors: true,            // добавить CORS-заголовки
+    hmr: {
+      host: '5.129.203.40', // ваш внешний IP или домен
+      protocol: 'ws',
+      port: 5173,
+    },
+  },
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css','resources/js/app.js'],
+      refresh: true,
+    }),
+  ],
 });
+
